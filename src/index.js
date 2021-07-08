5e8f1743c48982b64b79da2a3a2c17966a9c6773
Dohyeong Kim <o2kim0225@naver.com>
2021-07-08 22:26:41 +0900

    함수의 getter,setter

--- src/index.js
+++ src/index.js
@@ -23,3 +23,48 @@ function print2(hero) {
 
 print(ironMan)
 print2(captainAmerica)
+
+const dog = {
+    name: '멍멍이',
+    sound: '멍멍',
+    say: function () {
+        console.log(this.sound)
+    }
+}
+
+dog.say()
+
+const number = {
+    _a: 1,
+    _b: 2,
+    _sum: 3,
+    calculate() {
+        console.log('calculate')
+        this._sum = this._a + this._b
+    },
+    get a() {
+        return this._a
+    },
+    get b() {
+        return this._b
+    },
+    get sum() {
+        return this._sum
+    },
+    set a(value) {
+        console.log('a')
+        this._a = value
+        this.calculate()
+    },
+    set b(value) {
+        console.log('b')
+        this._b = value
+        this.calculate()
+    }
+}
+
+number.a = 1
+number.b = 2
+console.log(number.a)
+console.log(number.b)
+console.log(number.sum)
